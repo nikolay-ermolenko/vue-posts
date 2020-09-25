9c30f06efbcf627b94747abc2c4750e6c236cee2
nikolay-ermolenko <forum@terni.ru>
2020-09-25 13:59:48 +0300

    add rxjs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
   <v-app>
-    <h1>Hello vue (docs)</h1>
+    <v-main>
+      <v-container class="fill-heightd">
+        <v-row align="center">
+          <h3>Hello vue ({{ text }})</h3>
+        </v-row>
+        <v-row align="center">      
+            <v-text-field
+              ref="input"
+              label="Name"
+              @input="onTextFieldChange" />
+        </v-row>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Base from '@/common/Base.vue'
 import Component from 'vue-class-component'
+import { BehaviorSubject } from 'rxjs'
+import { debounceTime } from 'rxjs/operators'
 
 @Component
-export default class App extends Vue {
+export default class App extends Base {
+  private buffer: number = 500
+  private defaultValue: string = 'DEFAULT'
+  private inputValue: string = ''
+  private text: string = ''
 
+  private subject$: BehaviorSubject<string> 
+    = new BehaviorSubject<string>(this.defaultValue)
+
+  private created(): void {
+    this.observerSafe$(
+        this.subject$
+    ).pipe(
+       debounceTime(this.buffer),
+    ).subscribe((value: string) => this.text = value)
+  }
+
+  private onTextFieldChange(value: string): void {    
+    this.subject$.next(value || this.defaultValue)
+  }
 }
 </script>
