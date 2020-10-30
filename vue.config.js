b350f5f096345d025278745a217b07625d35687e
nikolay-ermolenko <forum@terni.ru>
2020-10-30 14:08:59 +0300

    add webpack-bundle-analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const BundleAnalyzerPlugin 
+  = require ('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
   runtimeCompiler: true,
   publicPath: './',
@@ -5,4 +8,17 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  configureWebpack: { 
+    plugins: (() => {
+      var res = [];
+
+      if (process.env.NODE_ENV === 'production') {
+        res.push(new BundleAnalyzerPlugin ({
+          analyzerMode: 'static',
+          openAnalyzer: false,
+        }));
+      }
+      return res;
+    })()
+  }, 
 };
