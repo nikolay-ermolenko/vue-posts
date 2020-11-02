127bfc55be2915cb13c4afb40b6b3066d8c1a76d
nikolay-ermolenko <forum@terni.ru>
2020-11-02 11:00:41 +0300

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,17 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        mjs: 'never',
+      },
+    ],
   },
   overrides: [
     {
