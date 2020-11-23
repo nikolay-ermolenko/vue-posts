32c36980c4a96bc656b3cd1e024d459efe497b3b
nikolay-ermolenko <forum@terni.ru>
2020-11-24 00:25:28 +0300

    add consts to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,21 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+import HomeView from '../views/Home.vue';
 
 Vue.use(VueRouter);
+
+const Home = 'Home';
+const About = 'About';
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: Home,
+    component: HomeView,
   },
   {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    name: About,
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
 ];
@@ -25,3 +25,8 @@ const router = new VueRouter({
 });
 
 export default router;
+
+export {
+  Home,
+  About,
+};
